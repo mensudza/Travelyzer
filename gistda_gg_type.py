ee35a91f89a19d99b20e355eb6f861aafbf1fcee
Sukanya Chinwicha <74386100+mensudza@users.noreply.github.com>
2025-11-20 05:06:11 +0700

    remove activity and add gistda and google place type

---
+++ gistda_gg_type.py
@@ -0,0 +1,25 @@
+pType_dictionary = {
+ 'Food': ['coffee_shop','cafe', 'restaurant','food','bakery','ice_cream_shop', 'seafood_restaurant', 'italian_restaurant', 'breakfast_restaurant', 'thai_restaurant'],
+ 'Bank/ATM': ['finance', 'bank',],
+ 'Business': [],
+ 'Education': ['university', 'school', 'secondary_school', 'primary_school'],
+ 'Religion': ['place_of_worship','church'],
+
+ 'Hospital/Healthcare': ['pharmacy', 'health','doctor'],
+ 'Conference/Community': ['convention_center','community_center'],
+ 'Automobile': ['gas_station', 'rest_stop', 'parking'],
+ 'Transportation': ['subway_station', 'airport','train_station','bus_stop',  'transit_station','marina',],
+ 'Service': ['real_estate_agency',],
+
+ 'Hotel': ['lodging', 'resort_hotel', 'hotel','hostel', 'bed_and_breakfast', 'cottage', 'premise'],
+ 'Infrastructure/Hydro': ['intersection'],
+ 'City Center': [],
+ 'Residence': [],
+ 'Shops/Store': ['grocery_store', 'shopping_mall',  'park', 'aquarium','supermarket', 'market','department_store', 'store','liquor_store','wholesaler'],
+
+ 'Tourism': ['point_of_interest','travel_agency','zoo','tourist_attraction', 'national_park', 'farm','event_venue', 'amusement_park'],
+ 'Entertainment': ['hiking_area',  'stadium', 'amusement_center'],
+ 'Art/Culture': ['museum', 'historical_landmark',],
+ 'Sport': ['sports_complex',   'sports_club', 'gym', 'athletic_field'],
+ 'Government': ['local_government_office'],
+ }
